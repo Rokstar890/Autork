556cff074b37482f847f40cd2c6a7516b43ed5d4
Rokstar890 <144723163+Rokstar890@users.noreply.github.com>
2024-01-27 00:52:37 +0530

    Update info.py

--- info.py
+++ info.py
@@ -119,7 +119,7 @@ if len(BIN_CHANNEL) == 0:
     exit()
 else:
     BIN_CHANNEL = int(BIN_CHANNEL)
-URL = environ.get("URL", "https://ckkdujj-8096c2a81552.herokuapp.com/")
+URL = environ.get("URL", "ckkdujj-8096c2a81552.herokuapp.com")
 if len(URL) == 0:
     print('Error - URL is missing, exiting now')
     exit()
